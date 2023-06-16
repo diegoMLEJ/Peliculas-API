66637a3a66129c66a4dddd3b11d2105333cb2778
SergioRuiz719 <126711109+SergioRuiz719@users.noreply.github.com>
2023-06-16 01:30:31 -0600

    Update usuarioRuta.js

--- rutas/usuarioRuta.js
+++ rutas/usuarioRuta.js
@@ -3,7 +3,6 @@ var express = require('express');
 var usuarioControl = require('../control/usuarioControl.js');
 var api = express.Router();
 
-var multipart = require('connect-multiparty');
 
 //Solicitudes para consultas 
 api.post('/registrar', usuarioControl.registrarUsuario);
